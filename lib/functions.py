122e1e767defe6180ebe9b68aa285703b023499d
ibrahimmohamed3 <ibra39167271@gmail.com>
2023-08-15 19:55:48 +0300

    complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
+def greeting():
+    print(f"Hello, {name}")
+greeting()   
+
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("Ibrahim")    
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}")
+greet_with_default("Ibrahim")    
 
 def add(num1, num2):
-    pass
+    print(num1+num2)
+add(2, 3)    
 
 def halve(number):
-    pass
+    print(number/2)
+halve(6)  
